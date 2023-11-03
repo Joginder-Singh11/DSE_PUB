358ccc75b67b42d042c31b4c33b4f50dca59a7e8
Joginder-Singh11 <joginder.singh11jan@gmail.com>
2023-11-03 16:49:25 +0530

    Updated

--- assets/student-profile/index.ts
+++
@@ -1,9 +0,0 @@
-import Building from './Building.svg';
-import Calendar from './Calendar.svg';
-import Card from './Card.svg';
-import Check from './Check.svg';
-import Gender from './Gender.svg';
-import Number from './Number.svg';
-import Pencil from './Pencil.svg';
-
-export { Building, Calendar, Card, Check, Number, Gender, Pencil }
